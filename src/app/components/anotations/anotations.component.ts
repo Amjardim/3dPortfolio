c05598645c92389e9ff8f1d0f429796c32555940
Antonio Jardim <amachadojardim@gmail.com>
2025-10-29 20:24:23 -0300

    Inital Setup

---
+++ src/app/components/anotations/anotations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anotations',
+  standalone: true,
+  imports: [],
+  templateUrl: './anotations.component.html',
+  styleUrl: './anotations.component.scss'
+})
+export class AnotationsComponent {
+
+}
