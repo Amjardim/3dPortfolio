bb49190cf6ec20209f8825c6f15c8a5d21cf2992
Antonio Jardim <amachadojardim@gmail.com>
2025-10-30 16:35:10 -0300

    Initial Setup 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CanvasComponent } from "./components/canvas/canvas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '3dPortfolio';
 }
--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-canvas',
   standalone: true,
   imports: [],
   templateUrl: './canvas.component.html',
-  styleUrl: './canvas.component.scss'
+  styleUrls: ['./canvas.component.scss']
 })
-export class CanvasComponent {
+export class CanvasComponent implements OnInit {
+  public scene!: THREE.Scene;
+  public camera!: THREE.PerspectiveCamera;
+  public renderer!: THREE.WebGLRenderer;
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly document = inject(DOCUMENT);
 
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.init();
+    }
+  }
+
+  private init(): void {
+    const container = this.document.createElement('div');
+    this.document.body.appendChild(container);
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+    this.camera.position.set(-1.8, 0.6, 2.7);
+
+    this.scene = new THREE.Scene();
+
+    const loader = new GLTFLoader().setPath('assets/');
+    loader.load('old_computers.glb', async (glb) => {
+      const model = glb.scene;
+      await this.renderer.compileAsync(model, this.camera, this.scene);
+      this.scene.add(model);
+      this.render();
+      });
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 1;
+    container.appendChild(this.renderer.domElement);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.addEventListener('change', () => this.render());
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.target.set(0, 0, -0.2);
+    controls.update();
+
+    window.addEventListener('resize', () => this.onWindowResize());
+  }
+
+  private onWindowResize(): void {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.render();
+  }
+
+  private render(): void {
+    this.renderer.render(this.scene, this.camera);
+  }
 }
